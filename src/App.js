35eeb4b144d240cd81211a5ba63422b749573f3e
Marie Sheela JOSEPH LOUIS SELVARAJ <sheelajoseph200@gmail.com>
2023-03-29 18:58:42 +0200

    Search component added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import { useState } from "react";
+import Search from "./components/Search";
+import './App.css';
+
 function App() {
-  return <div>
-    {console.log("rendering App")}
-    Welcome to React !
-  </div>
+  const [state, setState] = useState({
+    searchText: '',
+    cityData: {},
+    weatherData: {}
+  })
+  function handleSearchTextChange(e) {
+    setState((prevState) => {
+      return { ...prevState, searchText: e.target.value }
+    })
+  }
+  function handleSearchSubmit(e) {
+    e.preventDefault();
+  }
+  return <>
+    <header>
+      <div className="container">
+        <h1>WEATHER</h1>
+      </div>
+    </header>
+    <section className="main">
+      <div className="container">
+        <div className="d-flex">
+          <Search
+            searchText={state.searchText}
+            handleSearchTextChange={handleSearchTextChange}
+            handleSearchSubmit={handleSearchSubmit}
+          />
+        </div>
+      </div>
+    </section>
+  </>
 }
 export default App;
+
